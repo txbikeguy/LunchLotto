7c765aa2782da70bc1ce4b0d2346f3ce1deb9eb0
jkral1102 <32315185+jkral1102@users.noreply.github.com>
2018-03-15 16:16:33 -0500

    Created front page

---
+++ public/js/lunch.js
@@ -0,0 +1,9 @@
+
+$("#hero-btn").click(function(){
+    $("#content").slideDown("slow");
+})
+
+// Requires jquery plugin - http://flesler.blogspot.com/2009/05/jqueryscrollto-142-released.html
+// $("#hero-btn").click(function(){
+//     $("#hero-div").scrollTo("#content");
+// })
