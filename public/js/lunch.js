51db54513259145273521eaee05f5f870f00b217
unknown <jkral1101@gmail.com>
2018-03-16 18:24:49 -0500

    UI aesthetics updates + footer

--- public/js/lunch.js
+++ public/js/lunch.js
@@ -1,114 +1,116 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // On page refresh, scroll to top
-    $('html, body').animate({scrollTop: 0}, 'fast');
+    $('html, body').animate({ scrollTop: 0 }, 'fast');
 
 
-$("#hero-btn").click(function(){
-    // On hero button click, scroll to top of form
-    $('html, body').animate({scrollTop: $('.container').offset().top}, 'slow');
-});
+    $("#hero-btn").click(function () {
+        // On hero button click, scroll to top of form
+        $('html, body').animate({ scrollTop: $('.container').offset().top }, 'slow');
+    });
 
-// on make group button, scrolldown form
-$("#make-group-btn").click(function() {
-    $("#new-group-form").slideToggle();
-    // Hide Find Group form if open
-    $("#existing-group-form").slideUp(0);
-    // Hide Restaurant form if open
-    $("#restaurant-form").slideUp(0);
-});
+    // on make group button, scrolldown form
+    $("#make-group-btn").click(function () {
+        $("#new-group-form").slideToggle();
+        // Hide Find Group form if open
+        $("#existing-group-form").slideUp(0);
+        // Hide Restaurant form if open
+        $("#group-form").slideUp(0);
+    });
 
-// on find group button, scrolldown form
-$("#existing-group-btn").click(function() {
-    $("#existing-group-form").slideToggle();
-    // Hide New Group form if open
-    $("#new-group-form").slideUp(0);
-    // Hide Restaurant form if open
-    $("#restaurant-form").slideUp(0);
-});
+    // on find group button, scrolldown form
+    $("#existing-group-btn").click(function () {
+        $("#existing-group-form").slideToggle();
+        // Hide New Group form if open
+        $("#new-group-form").slideUp(0);
+        // Hide Restaurant form if open
+        $("#group-form").slideUp(0);
+    });
 
-// on submit new group, pull up restaurant form 
-$("#submit-new-grp").click(function() {
-    // Clear form values
-    $("#new-group-form input").val("");
-    // Hide New Group form
-    $("#new-group-form").slideUp(0);
-    // Toggle Restaurant form
-    $("#restaurant-form").slideToggle();
-});
+    // on submit new group, pull up restaurant form 
+    $("#submit-new-grp").click(function () {
+        // Clear form values
+        $("#new-group-form input").val("");
+        // Hide New Group form
+        $("#new-group-form").slideUp(0);
+        // Toggle Restaurant form
+        $("#group-form").slideToggle();
+    });
 
-$("#find-group").click(function() {
-    // Clear form values
-    $("#existing-group-form input").val("");
-    // Hide Find Group form
-    $("#existing-group-form").slideUp(0);
-    // Toggle Restaurant form
-    $("#restaurant-form").slideToggle();
-});
-
-
-//Declare global variables for Google Autocomplete 
-var globalCity;
-var globalState;
+    $("#find-group").click(function () {
+        // Clear form values
+        $("#existing-group-form input").val("");
+        // Hide Find Group form
+        $("#existing-group-form").slideUp(0);
+        // Toggle Restaurant form
+        $("#group-form").slideToggle();
+    });
 
 
-//Function to build the Yelp API call based on user suggestion
-function yelpSearchSettings(restaurantName) {
-    return {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurantName + "&limit=8",
-        "method": "GET",
-        "headers": {
-            "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
-            "Cache-Control": "no-cache",
-        }
-    };
-}
+    //Declare global variables for Google Autocomplete 
+    var globalCity;
+    var globalState;
 
-//Google Maps Autocomplete
-var input = document.getElementById('location');
-var autocomplete = new google.maps.places.Autocomplete(input, {
-    types: ['(cities)']
-});
 
-google.maps.event.addListener(autocomplete, 'place_changed', function () {
-    var place = autocomplete.getPlace();
-    var city = place.address_components[0].short_name;
-    var state = place.address_components[2].short_name;
-    var lat = place.geometry.location.lat();
-    var lng = place.geometry.location.lng();
-    globalCity = city;
-    globalState = state;
-});
-
-$("#submit-pick").click(function() {
-    event.preventDefault();
-    
-    // YELP SEARCH - calls Yelp API when a user enters a location and restaurant
-    function searchYelp() {
-        //Grab user input
-        var restaurantName = $("#restaurant-name").val().trim();
-        // var city = $("")
-        
-        //Empty all form values
-        // $("#existing-group-form")[0].reset();
-        $("#restaurant-form input").val("");
-       
-
-        var settings = yelpSearchSettings(restaurantName);
-
-        //Initiating Ajax call
-        $.ajax(settings).done(function (response) {
-            //Returns Yelp JSON for a restaurant 
-            var responseObject = response.businesses[0];
-            //databaseFunction(responseObject);
-            console.log(responseObject);
-        }); 
+    //Function to build the Yelp API call based on user suggestion
+    function yelpSearchSettings(restaurantName) {
+        return {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=" + globalCity + "," + globalState + "&term=" + restaurantName + "&limit=8",
+            "method": "GET",
+            "headers": {
+                "authorization": "Bearer UVD6_jknwmi1GkRxFFkWh7HX-JV_TrlieWHvaSMfi69lmcN6MPUUxk5EGJNnUWmLD3TV5vtrZ25whGiC9gmJRUbhEW5lz3Y6hOXT5oAS-WqK7U5TA772Lt4tBqlaWnYx",
+                "Cache-Control": "no-cache",
+            }
+        };
     }
-    searchYelp();
-})
 
+    //Google Maps Autocomplete
+    var input = document.getElementById('location');
+    var autocomplete = new google.maps.places.Autocomplete(input, {
+        types: ['(cities)']
+    });
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+        var city = place.address_components[0].short_name;
+        var state = place.address_components[2].short_name;
+        var lat = place.geometry.location.lat();
+        var lng = place.geometry.location.lng();
+        globalCity = city;
+        globalState = state;
+    });
+
+    $("#submit-pick").click(function () {
+        event.preventDefault();
+
+        // YELP SEARCH - calls Yelp API when a user enters a location and restaurant
+        function searchYelp() {
+            //Grab user input
+            var restaurantName = $("#restaurant-name").val().trim();
+            // var city = $("")
+
+            //Empty all form values
+            // $("#existing-group-form")[0].reset();
+            $("#group-form input").val("");
+
+
+            var settings = yelpSearchSettings(restaurantName);
+
+            //Initiating Ajax call
+            $.ajax(settings).done(function (response) {
+                //Returns Yelp JSON for a restaurant 
+                var responseObject = response.businesses[0];
+                //databaseFunction(responseObject);
+                console.log(responseObject);
+            });
+        }
+        searchYelp();
+    })
 
 
 
 }); // end of document.ready
+
+
+
