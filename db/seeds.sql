857726b13cba15e33b2433eadad5947e7841f656
Ben Cross <txbikeguy@gmail.com>
2018-03-22 18:08:19 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO lunches (group_name, user_name, restaurant_name) VALUES ('Blue', 'Alycia', 'Chipotle');
+INSERT INTO lunches (group_name, user_name, restaurant_name) VALUES ('Blue Baracudas', 'Alycia', 'Chipotle');
 INSERT INTO lunches (group_name, user_name, restaurant_name) VALUES ('Blue Baracudas', 'Ben', 'Pizza Luce');
 INSERT INTO lunches (group_name, user_name, restaurant_name) VALUES ('Blue Baracudas', 'Jen', 'Chilis');
 INSERT INTO lunches (group_name, user_name, restaurant_name) VALUES ('Blue Baracudas', 'Rob', 'Taco Bell');
