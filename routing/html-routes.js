a96ea97b46e5e338d946f4f88260cabc35bd4b67
unknown <jkral1101@gmail.com>
2018-03-23 22:30:07 -0500

    commit

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,9 +1,5 @@
 var path = require("path");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
     // HTML GET Requests
     // Below code handles when users "visit" a page.
