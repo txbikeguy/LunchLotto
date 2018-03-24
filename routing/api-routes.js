cf5a20d20cb1efd375018f9a0229f22b92d80f20
unknown <alyciarileym@gmail.com>
2018-03-23 20:34:21 -0500

    its pushing to the DB!

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -13,22 +13,29 @@ var sequelize = require("../config/connection.js");
 // =============================================================
 module.exports = function(app) {
 
-  // route "/" redirect
-  // app.get("/", function(req, res) {
-  //   res.redirect("/lunch");
-  // });  
 
-  // Get all books from a specific author
+  // Get the matching group from the search query
   app.get("/api/:groups", function(req, res) {
     if (req.params.groups) {
       sequelize.query("SELECT DISTINCT group_name FROM lunches AS lunch WHERE group_name='" + req.params.groups + "'", { raw: true
       }).then(function(results) {
         res.json(results);
-        console.log(results);
-        // console.log("group params", req.params.groups)
       });
     }
   });
 
   
+
+
+app.post("/api/new", function(req, res) {
+  console.log("Group Data:");
+  console.log(req.body);
+  Lunch.create({
+    group_name: req.body.group_name,
+    user_name: req.body.user_name,
+    restaurant_name: ""
+  });
+});
+
+
 };
